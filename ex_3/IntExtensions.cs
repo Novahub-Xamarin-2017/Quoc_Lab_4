bf7daef1d8b045aa50b1b2cbaddc865ea35c4e79
Steven <stevelee2497@gmail.com>
2017-11-18 14:15:31 +0700

    Update IntExtensions.cs

--- ex_3/IntExtensions.cs
+++ ex_3/IntExtensions.cs
@@ -7,7 +7,7 @@ namespace ex_3
         public static bool IsPrime(this int number)
         {
             if (number < 2) return false;
-            for (int i = (int)Math.Sqrt(number); i >= 2; i--)
+            for (var i = (int)Math.Sqrt(number); i >= 2; i--)
             {
                 if (number % i == 0)
                 {
