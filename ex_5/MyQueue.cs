cceafe7a1605d8539e5f7c80dc888c61b91ad227
Steven <stevelee2497@gmail.com>
2017-11-18 15:04:17 +0700

    add exercise 5

---
+++ ex_5/MyQueue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ex_5
+{
+    public class MyQueue<T>
+    {
+        private Node head, tail;
+        private int count;
+
+        public MyQueue()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public void Enqueue(T value)
+        {
+            Node newNode = new Node(value);
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                tail = newNode;
+            }
+            count++;
+        }
+
+        public T Dequeue()
+        {
+            if (head == null)
+            {
+                throw new Exception("Queue is Empty");
+            }
+            T result = head.Data;
+            head = head.Next;
+            count--;
+            return result;
+        }
+
+        public int Size() => count;
+
+        public bool IsEmpty() => head == null;
+
+        class Node
+        {
+            public T Data { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T data) => Data = data;
+        }
+    }
+}
