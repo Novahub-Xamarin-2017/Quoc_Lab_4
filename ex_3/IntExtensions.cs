dff0de5e3b7f39d6917d59391e29c3b2f1c76864
Steven <stevelee2497@gmail.com>
2017-11-18 13:43:06 +0700

    add exercise 3

---
+++ ex_3/IntExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex_3
+{
+    public static class IntExtensions
+    {
+        public static bool IsPrime(this int number)
+        {
+            if (number < 2) return false;
+            for (int i = (int)Math.Sqrt(number); i >= 2; i--)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
