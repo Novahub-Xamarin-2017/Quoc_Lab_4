09c2b72c9a4e54a383f00f30f077625cc0a9e571
Steven <stevelee2497@gmail.com>
2017-11-18 13:29:10 +0700

    add exercise 2

---
+++ ex_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ex_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string foo = "heLlO";
+            Console.WriteLine(foo.CaptionFirst());
+            Console.ReadKey();
+        }
+    }
+}
